9161bb4a674b824427ba48074c3f29dee0728880
rongjihuang <rongjihuang@gmail.com>
2012-05-10 00:14:43 +0800

    删除旧代码并创建测试工程

---
+++ src/cn/bliss/android/grabber/view/MainActivity.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package cn.bliss.android.grabber.view;
+
+import android.app.Activity;
+
+/**
+ * @author huangrongji
+ * 
+ */
+public class MainActivity extends Activity {
+	public int add(int a) {
+		return a++;
+	}
+}
---
+++ tests/src/cn/gm/android/grabber/test/ItemTest.java
@@ -0,0 +1,23 @@
+package cn.gm.android.grabber.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+import android.os.Environment;
+import cn.bliss.android.grabber.Item;
+
+public class ItemTest extends TestCase {
+	public void testExcute() throws IOException {
+		Item item = new Item();
+		item.setFrom("http://ww4.sinaimg.cn/bmiddle/9b8dcd88gw1dss5sp9tb8j.jpg");
+		File to = new File(Environment.getExternalStorageDirectory(),
+				"grabber/t.j");
+		assertFalse(to.exists());
+		item.setTo(to.getAbsolutePath());
+		item.excute();
+		assertTrue(to.exists());
+		to.delete();
+		assertFalse(to.exists());
+	}
+}
